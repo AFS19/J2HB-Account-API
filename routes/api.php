0b4a59f7f28681f1094f9065e24ebec1d6372976
AFS19 <iireco20@gmail.com>
2024-01-06 00:26:51 +0000

    reconfigure auth config:update guards api driver form api to jwt. and generate some routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::group(["middleware" => "auth:api"], function () {
+    Route::get('refresh', [AuthController::class, 'refreshToken'])->name("refreshToken");
+    Route::get('logout', [AuthController::class, 'logout'])->name("logout");
 });
